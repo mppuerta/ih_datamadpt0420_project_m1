485521bd2f04445dfa43a603eeec3f232fa5be46
Marta <martaperezpuerta@gmail.com>
2020-06-26 14:18:15 +0200

    dataset connected

--- p_acquisition/m_acquisition.py
+++ p_acquisition/m_acquisition.py
@@ -1,7 +1,26 @@
 import pandas as pd
+from sqlalchemy.pool import StaticPool
+from sqlalchemy import create_engine
 
 # acquisition functions
 
 def acquire():
-    data = pd.read_csv('./data/raw/vehicles.csv')
-    return data
+    engine = create_engine('sqlite:///../data/raw/raw_data_project_m1.db', poolclass = StaticPool)
+    rural = pd.read_sql('select\
+    country_info.country_code, career_info.normalized_job_code, country_info.rural\
+    from country_info\
+    join career_info\
+    on career_info.uuid = country_info.uuid',
+    con = engine)
+    rural.to_csv('../data/raw/rural_info_raw.csv', index = False)
+    return rural
+
+#This line is supposed to go to the main function:
+rural_info = acquire()
+
+
+
+
+
+
+
