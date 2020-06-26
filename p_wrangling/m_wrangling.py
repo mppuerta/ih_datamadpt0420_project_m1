25fc6d195b148ea063bbda1bca3662e02ff785f2
Marta <martaperezpuerta@gmail.com>
2020-06-27 01:00:16 +0200

    structuring the module

--- p_wrangling/m_wrangling.py
+++ p_wrangling/m_wrangling.py
@@ -5,25 +5,26 @@ def response_api(job_code):
     return requests.get(f'http://api.dataatwork.org/v1/jobs/{job_code}').json().get('title')
 
 
-# Function to get a dictionary containing every unique job code in the df. Its keys are job codes and its values are
-# job names.
-def jobs_to_dict(rural):
+# Function to clean job_title column. It gets a dictionary containing every unique job code in the df. Its keys are
+# job codes and its values are job names and iterates over the column.
+def jobs_column(rural):
     job_codes_list = list(rural['normalized_job_code'].dropna().unique())
-    return {job: response_api(job) for job in job_codes_list}
+    jobs_dict = {job: response_api(job) for job in job_codes_list}
+    rural['normalized_job_code'] = rural['normalized_job_code'].apply(lambda job: jobs_dict.get(job))
+    return rural
 
 
 # Function to clean rural column. The aim is to get only to values: 'rural' or 'urban'.
 def rural_column(rural):
     rural['rural'] = rural['rural'].str.lower()
     rural['rural'] = rural['rural'].str.replace('city', 'urban')\
-        .replace('non-rural','urban')\
+        .replace('non-rural', 'urban')\
         .replace('countryside', 'rural')\
         .replace('country', 'rural')
     return rural
 
 
 def wrangling(rural):
-    jobs_dict = jobs_to_dict(rural)
-    rural['normalized_job_code'] = rural['normalized_job_code'].apply(lambda job: jobs_dict.get(job))
-    rural_column_clean = rural_column(rural)
-    return rural_column_clean
+    jobs_column(rural)
+    rural_column(rural)
+    return rural
