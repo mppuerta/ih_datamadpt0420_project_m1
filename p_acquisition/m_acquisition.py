315a12cecfa70a1ece428ce3aae1cda4e98883f1
Marta <martaperezpuerta@gmail.com>
2020-06-27 00:48:52 +0200

    added describing comments to the functions

--- p_acquisition/m_acquisition.py
+++ p_acquisition/m_acquisition.py
@@ -3,16 +3,14 @@ from sqlalchemy.pool import StaticPool
 from sqlalchemy import create_engine
 
 
+# Function to connect the database and get a dataframe from it. It also exports it to a csv.
 def acquire(path):
-    engine = create_engine(f'sqlite:///{path}', poolclass = StaticPool)
+    engine = create_engine(f'sqlite:///{path}', poolclass=StaticPool)
     raw_rural = pd.read_sql('select\
     country_info.country_code, career_info.normalized_job_code, country_info.rural\
     from country_info\
     join career_info\
     on career_info.uuid = country_info.uuid',
-    con = engine)
-    raw_rural.to_csv('data/raw/raw_rural_info.csv', index = False)
+                            con=engine)
+    raw_rural.to_csv('data/raw/raw_rural_info.csv', index=False)
     return raw_rural
-
-
-
--- p_wrangling/m_wrangling.py
+++ p_wrangling/m_wrangling.py
@@ -1,14 +1,18 @@
 import requests
 
+# Function to build the API and make requests. Input: string, job code. Output: string, job name.
 def response_api(job_code):
     return requests.get(f'http://api.dataatwork.org/v1/jobs/{job_code}').json().get('title')
 
 
+# Function to get a dictionary containing every unique job code in the df. Its keys are job codes and its values are
+# job names.
 def jobs_to_dict(rural):
     job_codes_list = list(rural['normalized_job_code'].dropna().unique())
     return {job: response_api(job) for job in job_codes_list}
 
 
+# Function to clean rural column. The aim is to get only to values: 'rural' or 'urban'.
 def rural_column(rural):
     rural['rural'] = rural['rural'].str.lower()
     rural['rural'] = rural['rural'].str.replace('city', 'urban')\
