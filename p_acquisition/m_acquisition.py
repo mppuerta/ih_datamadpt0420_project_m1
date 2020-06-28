4204313f414db0cc56a62946f367192976b116de
Marta <martaperezpuerta@gmail.com>
2020-06-28 19:39:09 +0200

    web scrapping done

--- p_acquisition/m_acquisition.py
+++ p_acquisition/m_acquisition.py
@@ -5,14 +5,16 @@ from sqlalchemy import create_engine
 
 # Function to connect the database and get a dataframe from it. It also exports it to a csv.
 def acquire(path):
+    print('Connecting database...')
     engine = create_engine(f'sqlite:///{path}', poolclass=StaticPool)
-    raw_rural = pd.read_sql('select\
-    country_info.country_code, career_info.normalized_job_code, country_info.rural\
+    raw_rural = pd.read_sql("select\
+    country_info.country_code as 'Country', career_info.normalized_job_code as 'Job_name', country_info.rural as 'Rural'\
     from country_info\
     join career_info\
-    on career_info.uuid = country_info.uuid',
+    on career_info.uuid = country_info.uuid",
     con=engine)
     raw_rural.to_csv('data/raw/raw_rural_info.csv', index=False)
+    print('Database connected')
     return raw_rural
 
 
--- p_wrangling/m_wrangling.py
+++ p_wrangling/m_wrangling.py
@@ -1,5 +1,7 @@
 import requests
-import pandas as pd
+from bs4 import BeautifulSoup
+import lxml.html as lh
+import re
 
 # Function to build the API and make requests. Input: string, job code. Output: string, job name.
 def response_api(job_code):
@@ -9,24 +11,52 @@ def response_api(job_code):
 # Function to clean job_title column. It gets a dictionary containing every unique job code in the df. Its keys are
 # job codes, its values are job names it iterates over the column.
 def jobs_column(rural):
-    job_codes_list = list(rural['normalized_job_code'].dropna().unique())
+    print('Cleaning Job_name column...')
+    job_codes_list = list(rural['Job_name'].dropna().unique())
     jobs_dict = {job: response_api(job) for job in job_codes_list}
-    rural['normalized_job_code'] = rural['normalized_job_code'].apply(lambda job: jobs_dict.get(job))
+    rural['Job_name'] = rural['Job_name'].apply(lambda job: jobs_dict.get(job))
+    print('Job_name clean')
     return rural
 
 
 # Function to clean rural column. The aim is to get only two values: 'rural' or 'urban'.
 def rural_column(rural):
-    rural['rural'] = rural['rural'].str.lower()
-    rural['rural'] = rural['rural'].str.replace('city', 'urban')\
+    print('Cleaning Rural column...')
+    rural['Rural'] = rural['Rural'].str.lower()
+    rural['Rural'] = rural['Rural'].str.replace('city', 'urban')\
         .replace('non-rural', 'urban')\
         .replace('countryside', 'rural')\
         .replace('country', 'rural')
+    print('Rural column clean')
+    return rural
+
+
+def cleaning_list(name):
+    name1 = re.sub('<td>', '', str(name))
+    name2 = re.sub('</td>', '', str(name1))
+    return name2
+
+def countries_dict():
+    html = requests.get('https://iban.com/country-codes').content
+    soup = BeautifulSoup(html, 'lxml')
+    table = soup.find(id='myTable').find_all(['tbody'])
+    table_list = list(table[0].find_all('td'))
+    clean_table_list = list(map(cleaning_list, table_list))
+    table_dict = {clean_table_list[i + 1]: clean_table_list[i] for i in range(0, len(clean_table_list), 4)}
+    return table_dict
+
+def countries_clean(rural):
+    print('Cleaning countries column...')
+    countr_dict = countries_dict()
+    rural['Country'] = rural['Country'].apply(lambda country: countr_dict.get(country))
+    print('Countries column clean')
     return rural
 
 
 def wrangling(rural):
-    jobs_column(rural)
-    rural_column(rural)
-    return rural
+    rural_jobs_clean = jobs_column(rural)
+    rural_rural_clean = rural_column(rural_jobs_clean)
+    rural_countries_clean = countries_clean(rural_rural_clean)
+    return rural_countries_clean
+
 
